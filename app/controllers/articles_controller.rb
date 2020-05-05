5f65c5224847ad7e18801a06d11b79aa7cb34046
Laith Alquran <lmalquran@gmail.com>
2020-05-04 23:13:51 -0700

    Added articles list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def list
+  def index
     @articles = Article.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'articles'
-  resources  :articles, only: [:show]
+  resources  :articles, only: [:show, :index]
 end
